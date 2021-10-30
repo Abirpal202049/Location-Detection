37e558b17251b9b37fdadc08266ae6c97166002e
Abir Pal <abirpal282002@gmail.com>
2021-10-30 09:18:45 +0530

    Refactor the code

--- script.js
+++ script.js
@@ -17,18 +17,4 @@ function showPosition(position) {
   getapi(url);
   document.getElementById("lat").innerHTML = latitude ;
   document.getElementById("long").innerHTML = longitude;
-
-  // const successfulLookup = (position) => {
-  //   const { latitude, longitude } = position.coords;
-  //   fetch(
-  //     `https://api.opencagedata.com/geocode/v1/json?q=${latitude}+${longitude}&key=eccf45365d18446eac6f86bcbed3c2e5`
-  //   )
-  //     .then((response) => response.json())
-  //     .then(console.log);
-  // }; // Or do whatever you want with the result
-
-  // window.navigator.geolocation.getCurrentPosition(
-  //   successfulLookup,
-  //   console.log
-  // );
 }
